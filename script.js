b886392e4e3ba1ac531554c7e8d31573fe212a0c
Makau03 <makaumusembi3@gmail.com>
2024-10-15 14:57:54 +0300

    new

--- script.js
+++ script.js
@@ -46,3 +46,21 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+/**
+ * contact form
+ */
+
+const contactForm = document.querySelector(".contact-form");
+
+contactForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const name = document.querySelector('input[name="name"]').value;
+  const email = document.querySelector('input[name="email"]').value;
+  const message = document.querySelector('textarea[name="message"]').value;
+
+  const emailContent = `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}`;
+
+  window.location.href = `mailto:MakauMusembi3@gmail.com?subject=Portfolio Inquiry&body=${encodeURIComponent(emailContent)}`;
+});
